87291009e70ec104e10d68072db3fe03bd4c8cc9
Itz-Shadman <chowdhuryshadman707@gmail.com>
2025-11-13 06:40:32 +0000

    fourth commit

--- src/Components/TopRatedCars.jsx
+++ src/Components/TopRatedCars.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
-
+// TopRatedCars 
 const TopRatedCars = ({ search }) => {
   const [cars, setCars] = useState([]);
   const [loading, setLoading] = useState(true);
